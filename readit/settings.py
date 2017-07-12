dbaa56a3b3dc1f95c507f64d4f8706f8eb7b1aec
jay <jaycandidier@gmail.com>
2017-07-11 22:23:10 -0700

    my first edit

--- readit/settings.py
+++ readit/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# my firt edit sa code 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
